a69b5395f3245a8fae210f3ef9bae3f59797896b
munyite001 <emunyite@gmail.com>
2025-02-08 20:14:27 +0300

    Complete Implementation of Snippets CRUD API

--- routes/api.js
+++ routes/api.js
@@ -3,17 +3,18 @@ var router = express.Router();
 const { verifyToken } = require("../middleware/middleware");
 const usersController = require("../controllers/users.controller");
 const tagsController = require("../controllers/tags.controller");
+const snippetsController = require("../controllers/snippets.controller");
 
 // @route   GET /api
 // @desc    Redirect to /api/users
 router.get("/", (req, res) => {
-  res.send("Welcome to the API");
+    res.send("Welcome to the API");
 });
 
 // Users routes
 
 // @desc    Register a new user
-router.post("/auth/register",  usersController.registerUser);
+router.post("/auth/register", usersController.registerUser);
 
 // @desc    Login a user and get token
 router.post("/auth/login", usersController.loginUser);
@@ -29,8 +30,6 @@ router.put("/users/:id", verifyToken, usersController.updateUserById);
 
 // @desc    Delete a user by ID
 router.delete("/users/:id", verifyToken, usersController.deleteUserById);
-
-
 
 // Tags routes
 
@@ -52,6 +51,24 @@ router.put("/user/tags/:id", verifyToken, tagsController.updateTagById);
 // @desc    Delete a tag by ID
 router.delete("/user/tags/:id", verifyToken, tagsController.deleteUserTagById);
 
+// Snippets routes
 
+// @desc    Get all user snippets
+router.get("/snippets/all", verifyToken, snippetsController.getAllSnippets);
+
+// @desc    Get all user snippets
+router.get("/user/snippets/all", verifyToken, snippetsController.getAllUserSnippets);
+
+// @desc    Get a user snippet by ID
+router.get("/user/snippets/:id", verifyToken, snippetsController.getUserSnippetById);
+
+// @desc    Create a new user snippet
+router.post("/user/snippets", verifyToken, snippetsController.createUserSnippet);
+
+// @desc    Update a user snippet by ID
+router.put("/user/snippets/:id", verifyToken, snippetsController.updateUserSnippetsById);
+
+// @desc    Delete a user snippet by ID
+router.delete("/user/snippets/:id", verifyToken, snippetsController.deleteUserSnippetById);
 
 module.exports = router;
